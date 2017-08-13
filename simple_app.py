6e5b6c1ae1099cf88c6862b002c44c581947928f
curogihu <curoasg@gmail.com>
2017-08-14 06:22:02 +0900

    add render_template

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ def index(name='Treehouse'):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1 + num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template('add.html', **context)
 
 app.run(debug=True, port=8000, host='127.0.0.1')

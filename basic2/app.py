0794ffff8f16fce37c67690938f8133a84cdd304
curogihu <curoasg@gmail.com>
2017-08-14 08:34:13 +0900

    try using form in Flask

---
+++ basic2/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    return redirect(url_for('index'))
+
+app.run(debug=True, port=8000, host='127.0.0.1')

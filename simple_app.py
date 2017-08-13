ade913b0ea4db26230fcc5c4ac17de66db4394d5
curogihu <curoasg@gmail.com>
2017-08-13 23:28:19 +0900

    add multiple path to deal with int and float

--- simple_app.py
+++ simple_app.py
@@ -10,6 +10,9 @@ def index(name='Treehouse'):
 
 
 @app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
     return '{} + {} = {}'.format(num1, num2, num1 + num2)
 

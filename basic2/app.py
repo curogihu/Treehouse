33e38553319d3fcc067cd0831b985429704bfce9
curogihu <curoasg@gmail.com>
2017-08-15 08:27:34 +0900

    go advance to a course, "Build a Social Network with Flask"

--- basic2/app.py
+++ basic2/app.py
@@ -1,19 +1,45 @@
-from flask import Flask, render_template, redirect, url_for, request
+import json
+from flask import (Flask, render_template, redirect,
+                        url_for, request, make_response,
+                        flash)
+
+from options import DEFAULTS
 
 
 app = Flask(__name__)
+app.secret_key = 'fdakjfkajsajdljdfalfjaljfla'
+
+
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('character'))
+
+    except TypeError:
+        data = {}
+
+    return data
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', saves=get_saved_data())
+
+
+@app.route('/builder')
+def builder():
+    return render_template(
+                'builder.html',
+                saves=get_saved_data(),
+                options=DEFAULTS
+    )
 
 
 @app.route('/save', methods=['POST'])
 def save():
-    import pdb
-
-    pdb.set_trace()
-
-    return redirect(url_for('index'))
+    flash("Alright! That looks awesome!")
+    response = make_response(redirect(url_for('builder')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('character', json.dumps(data))
+    return response
 
 app.run(debug=True, port=8000, host='127.0.0.1')
